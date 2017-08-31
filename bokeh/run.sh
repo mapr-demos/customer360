6e217e8d3888b4c69737f73586e11751ab159266
iandow <kayaking@gmail.com>
2017-08-31 08:29:45 -0700

    add misc files to repo

--- bokeh/run.sh
+++ bokeh/run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 cd ~/customer360/bokeh
+export ODBCINI=/home/mapr/.odbc.ini
+export MAPRDRILLINI=/home/mapr/.mapr.drillodbc.ini
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib:/opt/mapr/drillodbc/lib/64:/usr/lib64:/opt/mapr/lib:/usr/lib/jvm/java-7-openjdk-amd64/jre/lib/amd64/server/
 bokeh serve . --allow-websocket-origin '*'
